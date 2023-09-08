f58b61d04c5f65d504b88821cc89472c3b47e729
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-08 16:36:04 +0330

    ba badbakhti G4

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -8,8 +8,6 @@ public class UserRepository {
     private Connection connection;
 
 
-
-
     public UserRepository() {
         try {
             this.connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "173725Hrm");
@@ -19,45 +17,49 @@ public class UserRepository {
 
     }
 
-    public int save(User user) throws SQLException{
-        String add ="INSERT INTO users( username, password, signup_date) values (?,?,?)";
-        PreparedStatement statement =connection.prepareStatement(add);
-        statement.setString(1 ,user.getUsername());
-        statement.setString(2,user.getPassword());
-        statement.setString(3,user.getSignup_date());
-        int result =statement.executeUpdate();
+    public int save(User user) throws SQLException {
+        String add = "INSERT INTO users( username, password, signup_date) values (?,?,?)";
+        PreparedStatement statement = connection.prepareStatement(add);
+        statement.setString(1, user.getUsername());
+        statement.setString(2, user.getPassword());
+        statement.setString(3, user.getSignup_date());
+        int result = statement.executeUpdate();
 
         return result;
 
     }
-    public int Update (User user) throws SQLException{
+
+    public int Update(User user) throws SQLException {
         String add = "UPDATE users set username='shayan' where username ='Hosein'";
-        PreparedStatement statement=connection.prepareStatement(add);
-        int result= statement.executeUpdate();
+        PreparedStatement statement = connection.prepareStatement(add);
+        int result = statement.executeUpdate();
         return result;
     }
-    public int Delete(User user) throws SQLException{
-        String add= " DELETE FROM users WHERE username='samyar'" ;
-        PreparedStatement statement =connection.prepareStatement(add);
-        int result =statement.executeUpdate();
-        return result;
-    }
-    public User loadAll() throws SQLException{
-        String add="SELECT * FROM users" ;
-        PreparedStatement statement =connection.prepareStatement(add);
-        ResultSet result =statement.executeQuery();
 
-        while (result.next()){
-            return new User(result.getInt(1),
+    public int Delete(User user) throws SQLException {
+        String add = " DELETE FROM users WHERE username='samyar'";
+        PreparedStatement statement = connection.prepareStatement(add);
+        int result = statement.executeUpdate();
+        return result;
+    }
+
+    public User[] loadAll() throws SQLException {
+        String add = "SELECT * FROM users";
+        PreparedStatement statement = connection.prepareStatement(add);
+        ResultSet result = statement.executeQuery();
+        User[] u = new User[7];
+        User user = new User();
+        int i = 0;
+
+        while (result.next()) {
+            u[i] = new User(result.getInt(1),
                     result.getString(2),
                     result.getString(3),
-                    result.getString(4)) ;
+                    result.getString(4));
+            i++;
         }
-        return null;
-
+        return u;
     }
-
-
 
 
 }
