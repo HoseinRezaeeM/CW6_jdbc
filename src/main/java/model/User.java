634944ae7f34e8bdc72479e8cb32b2df6880c618
HoseinRezaeeM <rezaeen77@gmail.com>
2023-09-08 11:26:46 +0330

    Write getter setter

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -5,4 +5,36 @@ public class User {
     private String username;
     private String password;
     private String sign_data;
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSign_data() {
+        return sign_data;
+    }
+
+    public void setSign_data(String sign_data) {
+        this.sign_data = sign_data;
+    }
 }
